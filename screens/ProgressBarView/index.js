52c9f4c89fb4b6e0b687ff5a79dd053636eef2e1
RicardoHernandezVarela <rhvarela011192@gmail.com>
2020-12-08 21:02:46 -0600

    Testing the ProgressBar component

--- screens/ProgressBarView/index.js
+++ screens/ProgressBarView/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, TextInput, StyleSheet} from 'react-native';
 
 import Header from '../../components/Header';
 import ProgressBar from '../../components/ProgressBar';
@@ -17,31 +17,94 @@ const styles = StyleSheet.create({
     height: height,
     backgroundColor: 'white',
   },
+  /* TITLE STYLES */
+  title: {
+    width: width,
+    marginTop: 20,
+    marginBottom: 10,
+    alignItems: 'center',
+  },
+  titleText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'gray',
+    textAlign: 'center',
+  },
   progressBarComponent: {
     width: width,
-    height: height - 55,
+    height: 100,
+    marginTop: 20,
+  },
+  /* PROGRESS CONTROL STYLES */
+  progressInput: {
+    width: width,
+    alignSelf: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  progressTextInput: {
+    width: width / 1.5,
+    borderBottomWidth: 0.5,
+    borderColor: Colors.silver,
+    textAlign: 'center',
+    paddingBottom: 5,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'gray',
+  },
+  progressLabel: {
+    fontSize: 14,
+    color: 'gray',
+    textAlign: 'center',
+    paddingTop: 5,
   },
 });
 
 class ProgressBarView extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      inputValue: '0',
+      progress: 0,
+    };
   }
 
   render() {
+    const {inputValue, progress} = this.state;
     const {viewName} = this.props;
 
     return (
       <View style={styles.mainContainer}>
+        {/*HEADER */}
         <Header viewName={viewName} elevation={5} />
+
+        {/* TITLE */}
+        <View style={styles.title}>
+          <Text style={styles.titleText}>
+            {'Testing the ProgressBar component'}
+          </Text>
+        </View>
+
+        {/* PROGRESS CONTROL */}
+        <View style={styles.progressInput}>
+          <TextInput
+            value={inputValue}
+            onChangeText={(text) => this.setState({inputValue: text})}
+            onSubmitEditing={() =>
+              this.setState({progress: parseFloat(inputValue, 10)})
+            }
+            style={styles.progressTextInput}
+          />
+          <Text style={styles.progressLabel}>{'Enter a number'}</Text>
+        </View>
 
         {/* ProgressBar COMPONENT */}
         <View style={styles.progressBarComponent}>
           <ProgressBar
             barWidth={200}
             barHeight={25}
-            percentage={95}
+            percentage={progress}
             colors={['white', Colors.cerulean]}
           />
         </View>
